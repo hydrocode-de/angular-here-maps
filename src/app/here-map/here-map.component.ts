8e54fd050de057f8382b3894a228ed8ead311490
Mirko Mälicke <mirko@maelicke-online.de>
2020-10-14 11:09:41 +0200

    small adaption

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 declare var H: any;
 
