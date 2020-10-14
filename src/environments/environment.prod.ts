e6dca7187bc25715c637772cf207529f20469203
Mirko Mälicke <mirko@maelicke-online.de>
2020-10-14 09:41:56 +0200

    added apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  hereAPIKey: 'aYGnuB4DJov0iqgQUydv4X-J0srdyqttNa8JabrwJ2w'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hereAPIKey: 'aYGnuB4DJov0iqgQUydv4X-J0srdyqttNa8JabrwJ2w'
 };
 
 /*
